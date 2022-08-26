ab150a53b4c09319cc5f6b961b4ad424b03e8eb3
Samue1A <92115015+Samue1A@users.noreply.github.com>
2022-08-26 23:43:09 +0100

    Add files via upload

--- DebbateWinner/main.js
+++ DebbateWinner/main.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('form').onsubmit = function() {
-        const word = document.querySelector('#wrd').ariaValueText;
-        alert(word)
+        const word = document.querySelector('#wrd').value;
+        const header = document.querySelector('h1');
+        document.querySelector('h1').innerHTML = word;
+
+
     }
 });
