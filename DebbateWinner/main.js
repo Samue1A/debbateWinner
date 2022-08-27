5bf674748f430c2f3f74b25a9cc3d4671f1dc554
Samue1A <92115015+Samue1A@users.noreply.github.com>
2022-08-27 08:38:41 +0100

    Delete DebbateWinner directory

--- DebbateWinner/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').onsubmit = function() {
-        const word = document.querySelector('#wrd').value;
-        const header = document.querySelector('h1');
-        document.querySelector('h1').innerHTML = word;
-
-
-    }
-});
